fb90e6d5b19a82b5c154e249a6d86f7a40197d7f
JACedwards <98490993+JACedwards@users.noreply.github.com>
2023-01-23 10:45:12 -0500

    mod_range_practice

--- matrices.py/longestPalindrome.py
+++ matrices.py/longestPalindrome.py
@@ -19,48 +19,48 @@
 #   store running count of longest
 
 
-def longest_palindrome (s):
+# def longest_palindrome (s):
 
-    l=0
-    r=0
-    output = 0
-    length = 0
+#     l=0
+#     r=0
+#     output = 0
+#     length = 0
 
-    while l-r !=1 and r != len(s)-1:
+#     while l-r !=1 and r != len(s)-1:
           
-        l_value = s[l]
-        s = s[l:]
-        r = s.find(l_value) + 1
-        while r-l >=1:
-            if s[l] == s[r] and r-l == 1:
-                length = len(s[l:r]) + 1
-                if output < length:
-                    output = length
-                    print(f'this is output: {output}')
-                    s=s[l+1:]
-                    l_value = s[l]
-                    s = s[l+1:]
-                    r = s.find(l_value) + 1
+#         l_value = s[l]
+#         s = s[l:]
+#         r = s.find(l_value) + 1
+#         while r-l >=1:
+#             if s[l] == s[r] and r-l == 1:
+#                 length = len(s[l:r]) + 1
+#                 if output < length:
+#                     output = length
+#                     print(f'this is output: {output}')
+#                     s=s[l+1:]
+#                     l_value = s[l]
+#                     s = s[l+1:]
+#                     r = s.find(l_value) + 1
                     
 
-            elif s[l] != s[r]:
-                r+=1
+#             elif s[l] != s[r]:
+#                 r+=1
                 
-            else:
-                length = len(s[l:r]) + 1
-                if output < length:
-                    output = length
-                    print(f'this is output: {output}')
-                    s=s[l+1:]
-                    l_value = s[l]
-                    s = s[l+1:]
-                    r = s.find(l_value) + 1
+#             else:
+#                 length = len(s[l:r]) + 1
+#                 if output < length:
+#                     output = length
+#                     print(f'this is output: {output}')
+#                     s=s[l+1:]
+#                     l_value = s[l]
+#                     s = s[l+1:]
+#                     r = s.find(l_value) + 1
     
-    return output
+#     return output
 
 
 
-print(longest_palindrome("zzbaabcd"))
+# print(longest_palindrome("zzbaabcd"))
 
 
 
@@ -85,23 +85,52 @@ print(longest_palindrome("zzbaabcd"))
 # test.assert_equals(longest_palindrome("baablkj12345432133d"), 9)
 
 
+def longest_palindrome (s):
 
-    # if len(s) % 2 != 0:
-    #     while r-l >=1:
-    #         if s[l] != s[r]:
-    #             return "this ain't no palindrome"
-    #         else:
-    #             l += 1
-    #             r -= 1
-    #     return 'This is a palindrome'
-    # else:
-    #     while r-l >=1:
-    #         if s[l] != s[r]:
-    #             return "this ain't no palindrome"
-    #         else:
-    #             l += 1
-    #             r -= 1
-    #     return 'This is a palindrome'
+    length = 0
+    output = 0
+    l = 0
+    r = 2
+    while l == len(s) - 2 and r != len(s) - 1:
+
+        l = 0
+        r = 1
+        left = 0
+        right = 1
+        slce = s[left:right]
+        if len(slce) % 2 != 0:
+            while r-l >=1:
+                if slce[l] != slce[r]:
+                    output = length
+                    left += 1
+                    right = left + 2
+                    slce = s[left:right]
+                else:
+                    l += 1
+                    r -= 1
+            if output < length:
+                output = length
+                left += 1
+                right = left + 2
+                slce = s[left:right]
+        else:
+            while r-l >=1:
+                if slce[l] != slce[r]:
+                    output = length
+                    left += 1
+                    right = left + 2
+                    slce = s[left:right]
+                else:
+                    l += 1
+                    r -= 1
+            output = length
+            left += 1
+            right = left + 2
+            slce = s[left:right]
+    return output
+
+
+print(longest_palindrome("zzbaabcd"))
 # _______________________________
     # flag = True
     # while flag == True:
